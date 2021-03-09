ba1d50ad9b81d43b1648580babc368f16aff55aa
usamasarfraz <usamasarfraz822@gmail.com>
2021-03-09 13:08:42 +0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -36,7 +36,6 @@ defmodule ElixirDemo.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"}
     ]
   end
